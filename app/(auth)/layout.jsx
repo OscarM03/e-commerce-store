44b28a4999d00d11ed6079fffff98b93d97371de
OscarM03 <oscarmutwiri03@gmail.com>
2025-03-20 09:44:32 +0300

    Created the auth folder with the sign in and sign up form

---
+++ app/(auth)/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/navigation/Navbar'
+
+const layout = ({children}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default layout
